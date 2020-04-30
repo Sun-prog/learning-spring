a7ed679e3a4e666f2aee8cd352469eee6eae0288
Tatiana Zh <gruppa15@mail.ru>
2020-05-01 00:00:42 +0300

    Initial commit

---
+++ src/main/java/com/example/learningspring/controller/MessageController.java
@@ -0,0 +1,22 @@
+package com.example.learningspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First"); }});
+        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second"); }});
+        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third"); }});
+    }};
+    @GetMapping
+    public List<Map<String, String>> list(){return messages;}
+}
